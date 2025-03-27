d64abb3f9b72d1b4316cb1e7f29f60b319138702
pylaa <apyla17@gmail.com>
2025-03-27 17:36:34 -0500

    Added test cases

--- app/src/androidTest/java/com/example/rickapp/MainScreenTest.kt
+++ app/src/androidTest/java/com/example/rickapp/MainScreenTest.kt
@@ -1,34 +1,66 @@
 package com.example.rickapp
 
 import androidx.activity.ComponentActivity
-import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
-import androidx.compose.ui.test.onNodeWithTag
 import androidx.navigation.compose.rememberNavController
 import com.example.rickapp.presentation.CharacterViewModel
 import com.example.rickapp.presentation.MainScreen
+import com.example.rickapp.repository.CharacterRepository
 import com.example.rickapp.ui.theme.RickAppTheme
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
-class MainScreenTest {
+class MainScreenUITest {
 
     @get:Rule
     val composeTestRule = createAndroidComposeRule<ComponentActivity>()
 
+    private lateinit var viewModel: CharacterViewModel
+
+    @Before
+    fun setup() {
+        val repo = (composeTestRule.activity.application as App).getCharacterRepository()
+        viewModel = CharacterViewModel(repo)
+    }
+
     @Test
     fun searchBar_isDisplayed() {
-        val app = composeTestRule.activity.application as App
-        val viewModel = CharacterViewModel(app.getCharacterRepository())
-
         composeTestRule.setContent {
             RickAppTheme {
                 MainScreen(viewModel = viewModel, navController = rememberNavController())
             }
         }
 
-        composeTestRule
-            .onNodeWithTag("SearchTextField")
-            .assertIsDisplayed()
+        composeTestRule.onNodeWithTag("SearchTextField").assertIsDisplayed()
+    }
+
+    @Test
+    fun filterButtons_areDisplayed() {
+        composeTestRule.setContent {
+            RickAppTheme {
+                MainScreen(viewModel = viewModel, navController = rememberNavController())
+            }
+        }
+
+        composeTestRule.onNodeWithText("Status").assertIsDisplayed()
+        composeTestRule.onNodeWithText("Species").assertIsDisplayed()
+        composeTestRule.onNodeWithText("Type").assertIsDisplayed()
+    }
+
+    @Test
+    fun characterCard_displaysCharacterName() {
+        composeTestRule.setContent {
+            RickAppTheme {
+                MainScreen(viewModel = viewModel, navController = rememberNavController())
+            }
+        }
+
+        composeTestRule.waitUntil(timeoutMillis = 5000) {
+            composeTestRule.onAllNodesWithText("Rick Sanchez").fetchSemanticsNodes().isNotEmpty()
+        }
+
+        composeTestRule.onNodeWithText("Rick Sanchez").assertIsDisplayed()
     }
 }
